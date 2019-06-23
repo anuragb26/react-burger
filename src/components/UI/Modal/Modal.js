be60800a0fc67b1d395af9716eaea8641f81fcf2
Anurag Bajaj <anuragb26@gmail.com>
2019-06-23 12:00:01 +0530

    Chore:Use React.memo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react'
-import Classes from './Modal.css'
-import Aux from '../../../hoc/Wrapper'
-import Backdrop from '../Backdrop/Backdrop'
+import React, { Component } from "react";
+import Classes from "./Modal.css";
+import Aux from "../../../hoc/Wrapper";
+import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate (nextProps, nextState) {
-    return (nextProps.show !== this.props.show) || (nextProps.children !== this.props.children)
-  }
+const Modal = props => {
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={Classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0"
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
-  componentDidUpdate () {
-
-  }
-
-  render () {
-    return (
-      <Aux>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div className={Classes.Modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0'
-          }}>
-          {this.props.children}
-        </div>
-      </Aux>
-    )
-  }
-}
-
-export default Modal
+export default React.memo(Modal, (prevProps, nextProps) => {
+  return (
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+  );
+});
